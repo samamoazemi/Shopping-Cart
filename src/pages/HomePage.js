e9121f2af6ba39c21c581495f46484c3633aacb7
samamoazemi <moazemisama@gmail.com>
2022-02-07 02:26:24 -0800

    Map on products

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,25 @@
 import Layout from "../Layout/Layout";
+import * as data from "../data";
 
 const HomePage = () => {
     return ( 
         <Layout>
-            <main>
-                <section>product list ...</section>
+          <main className="container">
+            <section className="productList">
+              {data.products.map((product) => {
+                return(
+                 <section className="product">
+                   <div className="productImg">
+                     <img src={product.image} alt={product.name}></img>
+                   </div>
+                   <div className="productDesc">
+                     <p>{product.name}</p>
+                     <p>$ {product.price}</p>
+                  </div>
+                 </section>
+                )
+              })}
+            </section>
             </main>
         </Layout>
      );
