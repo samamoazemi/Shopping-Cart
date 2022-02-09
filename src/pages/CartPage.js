b7dff184a310680a8d65899d9cdbbd0c3fd326d4
samamoazemi <moazemisama@gmail.com>
2022-02-09 04:21:26 -0800

    Styling the cart page 1

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,23 +1,42 @@
 import Layout from "../Layout/Layout";
 import { useCart } from "../Providers/CartProvider";
+import "./cartPage.css";
 
 const CartPage = () => {
 
     const { cart } = useCart();
-    console.log(cart)
-    
-    return ( 
+
+    if(!cart.length)
+    return(
         <Layout>
             <main>
-                {cart.length ? (
-                    cart.map((item) => (
-                <div key={item.id}>
-                    <p>{item.name}</p>
-                </div>
-              ))
-            ) : (
-              <p>no item in cart !</p>
-            )}
+               <h2>cart is empty !</h2>
+            </main>
+        </Layout>
+    )
+
+    return ( 
+        <Layout>
+            <main className="container">
+                <section className="cartItemList">
+                    {cart.map((item) => {
+                        return(
+                         <div className="cartItem">
+                            <div className="itemImage">
+                                <img src={item.image} alt={item.name}></img>
+                            </div>
+                            <div>{item.name}</div>
+                            <div>{item.price * item.quantity}</div>
+                            <div>
+                                <button>Add</button>
+                                <button>{item.quantity}</button>
+                                <button>remove</button>
+                            </div>
+                         </div>
+                        )
+                    })}
+                </section>
+                <section className="cartSummery">cart summery</section>
             </main>
         </Layout>
      );
