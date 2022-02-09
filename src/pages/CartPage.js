0d57a51d8179b4e955e6df23c48024cd052ddc9b
samamoazemi <moazemisama@gmail.com>
2022-02-09 03:06:14 -0800

    Load cart products

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,9 +1,24 @@
 import Layout from "../Layout/Layout";
+import { useCart } from "../Providers/CartProvider";
 
 const CartPage = () => {
+
+    const { cart } = useCart();
+    console.log(cart)
+    
     return ( 
         <Layout>
-            <main>this is cart page !</main>
+            <main>
+                {cart.length ? (
+                    cart.map((item) => (
+                <div key={item.id}>
+                    <p>{item.name}</p>
+                </div>
+              ))
+            ) : (
+              <p>no item in cart !</p>
+            )}
+            </main>
         </Layout>
      );
 }
